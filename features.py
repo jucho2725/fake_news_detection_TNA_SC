98f5f33dd831f198448cee5cd09c003539608f71
JinUk, Cho <cju2725@gmail.com>
2019-06-26 00:35:54 +0900

    add features files

---
+++ features.py
@@ -0,0 +1,8 @@
+"""
+Extract features
+
+June 2nd, 2019
+author: Jin Uk, Cho
+"""
+
+import sklearn
---
+++ linguistic.py
@@ -0,0 +1,10 @@
+"""
+Add linguistic features
+
+lexical-semantic meanings
+TF, MI based on the corpus
+
+May 31th, 2019
+author: Jin Uk, Cho
+"""
+
