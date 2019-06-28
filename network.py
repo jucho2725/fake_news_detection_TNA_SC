c6f2741cd9f448fb88d49955d9f0def8bbaf0121
JinUk, Cho <cju2725@gmail.com>
2019-06-28 18:00:13 +0900

    edit lemmatization func, objectify visual class

--- network.py
+++ network.py
@@ -9,39 +9,55 @@ source : https://m.blog.naver.com/PostView.nhn?blogId=kiddwannabe&logNo=22115631
 """
 
 
-import pandas as pd
-import nltk
-from nltk.corpus import wordnet as wn
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk import sent_tokenize, word_tokenize, pos_tag
-from collections import defaultdict
-from nltk.corpus import stopwords
-from collections import Counter
+from collections import Counter, defaultdict
 from itertools import combinations
 
+import nltk
 import numpy as np
+import pandas as pd
 import sklearn.metrics as metrics
 import tensorflow as tf
+from nltk import sent_tokenize, word_tokenize, pos_tag, Text
+from nltk.collocations import BigramCollocationFinder
+from nltk.corpus import stopwords
+from nltk.corpus import webtext
+from nltk.corpus import wordnet as wn
+from nltk.metrics import BigramAssocMeasures
+from nltk.stem.wordnet import WordNetLemmatizer
 from tensorflow.python.data import Dataset
 
 
 class Processing():
     def __init__(self):
-        tag_map = defaultdict(lambda : wn.NOUN)
+        self.tag_map = defaultdict(lambda : wn.NOUN)
         # print(tag_map)
-        tag_map['J'] = wn.ADJ
-        tag_map['V'] = wn.VERB
-        tag_map['R'] = wn.ADV
+        self.tag_map['J'] = wn.ADJ
+        self.tag_map['V'] = wn.VERB
+        self.tag_map['R'] = wn.ADV
 
-        text1 = "guru99 is a kind of learning experiences."
+    # def word_lemma(self, word):
+    #     lemma_function = WordNetLemmatizer()
+    #     postag = pos_tag(word)
+    #     for lem, cat in postag:
+    #         tag = cat
+    #     if tag.startswith('J'):
+    #         return lemma_function.lemmatize(word, wn.ADJ)
+    #     elif tag.startswith('V'):
+    #         return lemma_function.lemmatize(word, wn.VERB)
+    #     elif tag.startswith('R'):
+    #         return lemma_function.lemmatize(word, wn.ADV)
+    #     elif tag.startswith('N'):
+    #         return lemma_function.lemmatize(word, wn.NOUN)
+    #     else:
+    #         return ''
 
     # 문장 하나 lemmatization 함수
-    def token_lemma1(self, text): # token에 is, 같은 애들을 be 로 변환 시키지 않음
+    def sent_lemma(self, text): # token에 is, 같은 애들을 be 로 변환 시키지 않음
         results = []
         tokens = word_tokenize(text)
-        lemma_function = WordNetLemmatizer()
-        for token in tokens:
-            lemma = lemma_function.lemmatize(token)
+        lmtzr = WordNetLemmatizer()
+        for token, tag in pos_tag(tokens):
+            lemma = lmtzr.lemmatize(token, self.tag_map[tag[0]])
             # print(token, "=>", lemma)
             results.append(lemma)
         return results
@@ -51,14 +67,14 @@ class Processing():
         lemma_data = []
         sentences = sent_tokenize(text)
         for sent in sentences:
-            lemma_sent = self.token_lemma1(sent)
+            lemma_sent = self.sent_lemma(sent)
             lemma_data.append(lemma_sent)
         return lemma_data
 
     # 불용어 처리 함수
     def stopword(self, sentences):
         stopWords = set(stopwords.words('english'))
-        added_stopword = ['“', '”', '.', ',']
+        added_stopword = ['“', '”', '.', ',', '-', "—", "'s", "n't", "''", ';', '&', "``", '?']
         results = []
 
         for sent in sentences:
@@ -71,7 +87,70 @@ class Processing():
         # print(results)
         return results
 
+    # # 연어 합치기
+    # def collocation(self, contents):
+    #     for sent in contents:
+    #         for w in sent:
+    #             bcf = BigramCollocationFinder.from_words(sent)
+    #             filter_stop = lambda w: len(w) < 3
+    #             bcf.apply_word_filter(filter_stop)
+    #             apply_list = bcf.nbest(BigramAssocMeasures.likelihood_ratio, 4)
+    #
+    #
+    #     return col_list
+
     # 태깅 함수
+
+    def apply_collocations(self, sentence):
+        set_colloc = set([("Donald", "Trump"), ("ABC", "News"), ("Hillary", "Clinton"), ("Chelsea", "Clinton")])
+        list_bigrams = list(nltk.bigrams(sentence))
+        # print("list bigram is : {0}".format(list_bigrams))
+        # print(list_bigrams[0][0])
+        # print(list_bigrams[0][1])
+        set_bigrams = set(list_bigrams)
+        intersect = set_bigrams.intersection(set_colloc)
+        # print(set_colloc)
+        # print(set_bigrams)
+        #  No collocation in this sentence
+        if not intersect:
+            return sentence
+        # At least one collocation in this sentence
+        else:
+            new_sentence = []
+            set_words_iters = set()
+            # Create set of words of the collocations
+            for bigram in intersect:
+                set_words_iters.add(bigram[0])
+                set_words_iters.add(bigram[1])
+            # print(set_words_iters)
+            # print("**")
+            # Sentence beginning
+            if list_bigrams[0][0] not in set_words_iters:
+                new_sentence.append(list_bigrams[0][0])
+                begin = 0
+            else:
+                new_word = list_bigrams[0][0] + '_' + list_bigrams[0][1]
+                new_sentence.append(new_word)
+                begin = 1
+
+            for i in range(begin, len(list_bigrams)):
+                # print(new_sentence)
+                if list_bigrams[i][1] in set_words_iters and list_bigrams[i] in intersect:
+                    new_word = list_bigrams[i][0] + '_' + list_bigrams[i][1]
+                    new_sentence.append(new_word)
+                elif list_bigrams[i][1] not in set_words_iters:
+                    new_word = list_bigrams[i][1]
+                    new_sentence.append(new_word)
+            return new_sentence
+
+
+    def collocate_content(self, contents):
+        results = []
+        for sent in contents:
+            new_sent = self.apply_collocations(sent)
+            results.append(new_sent)
+        return results
+
     def tag_content(self, contents):
         """
         Tag all words in content
@@ -96,10 +175,10 @@ class Processing():
 
         selected_results = []
 
-        for content in contents:
+        for sent in contents:
             selection = []
             # 단어를 lex, tag 를 cat 이라 표현
-            for lex, cat in content:
+            for lex, cat in sent:
                 if cat in tag_filter:
                     # tag 말고 안에 단어 lex 만 남겨야함
                     selection.append(lex)
@@ -145,30 +224,31 @@ class Processing():
         # return list_key_value
         return sorted_data
 
+""" 테스트 """
+# example_text = "The Trump administration will delay tariffs on cars and car part imports for up to six months as it negotiates trade deals with the European Union and Japan. In a proclamation Friday, Trump said he directed U.S.Trade Representative Robert Lighthizer to seek agreements to “address the threatened impairment” of national security from car imports. Trump could choose to move forward with tariffs during the talks. “United States defense and military superiority depend on the competitiveness of our automobile industry and the research and development that industry generates,” White House press secretary Sarah Huckabee Sanders said in a statement. “The negotiation process will be led by United States Trade Representative Robert Lighthizer and, if agreements are not reached within 180 days, the President will determine whether and what further action needs to be taken."
+# text = open("Donald.txt", encoding='utf-8').read()
+# print(text)
 
-# Example
-
-text = "The Trump administration will delay tariffs on cars and car part imports for up to six months as it negotiates trade deals with the European Union and Japan. In a proclamation Friday, Trump said he directed U.S.Trade Representative Robert Lighthizer to seek agreements to “address the threatened impairment” of national security from car imports. Trump could choose to move forward with tariffs during the talks. “United States defense and military superiority depend on the competitiveness of our automobile industry and the research and development that industry generates,” White House press secretary Sarah Huckabee Sanders said in a statement. “The negotiation process will be led by United States Trade Representative Robert Lighthizer and, if agreements are not reached within 180 days, the President will determine whether and what further action needs to be taken."
-
-N = Processing()
-lemed_content = N.lemma_whole(text)
+# N = Processing()
+# lemed_content = N.lemma_whole(text)
+# lemed_content = N.lemma_whole(example_text)
 # print(lemed_content)
 
-stopped_content = N.stopword(lemed_content)
+# stopped_content = N.stopword(lemed_content)
+# collocated_content = N.collocate_content(stopped_content)
+# print(collocated_content)
 
-tagged_results = N.tag_content(stopped_content)
+# tagged_results = N.tag_content(collocated_content)
 # print(tagged_results)
 # print('***************************************')
 
-# 어떤 태그들만 남길지
-tag_filter = ['NNP', 'NN', 'NNS', 'VBP', 'VBD', 'VBN', 'JJ', 'RB', 'VB']
-
-selected_results = N.select_results(tagged_results, tag_filter)
+# # 어떤 태그들만 남길지
+# tag_filter = ['NNP', 'NN', 'NNPS', 'NNS', 'VBG', 'VBP', 'VB', 'RB', 'VB']
+#
+# selected_results = N.select_results(tagged_results, tag_filter)
 # print(selected_results)
-
-final_result = N.create_cooc_mat(selected_results)
-
-
+#
+# final_result = N.create_cooc_mat(selected_results)
 # print(final_result)
 # print(final_result['Linkage'][0])
 
@@ -182,5 +262,6 @@ corpus2graph 로 다른 객체 하나 만들기 -
 이상한 단어 없에기 
 동사와 명사 관계 나타낼수 있는지
 
+Sent tokenizer 함수 보고 그에 맞게 기사 바꾸늑네 좋을듯
 
 """
--- visualization.py
+++ visualization.py
@@ -9,93 +9,165 @@ author: Jin Uk, Cho
 source : https://kiddwannabe.blog.me/221362423659
 """
 
-import networkx as nx
-import matplotlib.pyplot as plt
 import sys
 
-import network
+import matplotlib.pyplot as plt
+import networkx as nx
 import pandas as pd
+import plotly.plotly as py
+from plotly.graph_objs import *
 
-text = "The Trump administration will delay tariffs on cars and car part imports for up to six months as it negotiates trade deals with the European Union and Japan. In a proclamation Friday, Trump said he directed U.S.Trade Representative Robert Lighthizer to seek agreements to “address the threatened impairment” of national security from car imports. Trump could choose to move forward with tariffs during the talks. “United States defense and military superiority depend on the competitiveness of our automobile industry and the research and development that industry generates,” White House press secretary Sarah Huckabee Sanders said in a statement. “The negotiation process will be led by United States Trade Representative Robert Lighthizer and, if agreements are not reached within 180 days, the President will determine whether and what further action needs to be taken."
+import network
 
+# text = "The Trump administration will delay tariffs on cars and car part imports for up to six months as it negotiates trade deals with the European Union and Japan. In a proclamation Friday, Trump said he directed U.S.Trade Representative Robert Lighthizer to seek agreements to “address the threatened impairment” of national security from car imports. Trump could choose to move forward with tariffs during the talks. “United States defense and military superiority depend on the competitiveness of our automobile industry and the research and development that industry generates,” White House press secretary Sarah Huckabee Sanders said in a statement. “The negotiation process will be led by United States Trade Representative Robert Lighthizer and, if agreements are not reached within 180 days, the President will determine whether and what further action needs to be taken."
+text = open("Donald.txt", encoding='utf-8').read()
 # network 에서 호출하여 전처리
 N = network.Processing()
 lemed_content = N.lemma_whole(text)
 stopped_content = N.stopword(lemed_content)
-tagged_results = N.tag_content(stopped_content)
-
-tag_filter = ['NNP', 'NN', 'NNS', 'VBP', 'VBD', 'VBN', 'JJ', 'RB', 'VB']
-
+collocated_content = N.collocate_content(stopped_content)
+tagged_results = N.tag_content(collocated_content)
+tag_filter = ['NNP', 'NN', 'NNPS', 'NNS', 'VBG', 'VBP', 'VB', 'RB', 'VB']
 selected_results = N.select_results(tagged_results, tag_filter)
+
+# 그래프를 그리는데 사용된 co occurrence matrix 결과(dataframe 형태)
 final_result = N.create_cooc_mat(selected_results)
-
-print('The graph has {0} nodes'.format(len(final_result)))
-
-
-# 그래프 생성
-# MST(Minum Spanning Tree)-based graph
-
-def vis_graph(matrix, NETWORK_MAX):
-    """
-    Create Co-Occurrence Matrix
-    :param matrix: (list) co-occurence matrix data
-    :param NETWORK_MAX: (int) number of network ????
-    """
-    # 공부 더 필요
-    G = nx.Graph()
-    i = 0
-
-    # create edge
-    for i in range(len(matrix)):
-        # print('{0} is the number'.format(len(matrix)))
-        # print(matrix['Linkage'][i])
-        w1 = matrix['Linkage'][i][0]
-        w2 = matrix['Linkage'][i][1]
-        count = matrix['Weight'][i]
-        i += 1
-        if i > NETWORK_MAX: break
-
-        G.add_edge(w1, w2, weight=count)
-
-    # create MST model
-    T = nx.minimum_spanning_tree(G)
-    nodes = nx.nodes(T)
-    degrees = nx.degree(T)
-
-    # set size of node
-    node_size = []
-    for node in nodes:
-        ns = degrees[node] * 100
-        node_size.append(ns)
-
-    # font configuration
-    if sys.platform in ["win32", "win64"]:
-        font_name = "malgun gothic"
-    elif sys.platform == "darwin":
-        font_name = "AppleGothic"
-
-    # disply the result
-    plt.figure(figsize=(16, 12))
-    nx.draw_networkx(T,
-                     pos=nx.fruchterman_reingold_layout(G, k=0.5),
-                     node_size=node_size,
-                     node_color="yellow",
-                     font_family=font_name,
-                     label_pos=1,  # 0=head, 0.5=center, 1=tail
-                     with_labels=True,
-                     font_size=12
-                     )
-
-    # 그래프 생성
-    # plt.axis("off")
-    # plt.show()
-
-    return G
+print('The network has {0} edges'.format(len(final_result)))
+print(final_result)
 
 
-graph_ex = vis_graph(final_result, len(final_result))
+def label_result(selected_result):  # label들 확인
+    label = []
+    for sent in selected_result:
+        for i in sent:
+            if i not in label:
+                label.append(i)
+    return label
 
 
+label = label_result(selected_results)
+print("label is")
+print(label)
+
+
+class Visualization():
+    def __init__(self, matrix):
+        """
+        Visualize Co-Occurrence Matrix
+        :param matrix: (list) co-occurence matrix data
+        :param NETWORK_MAX: (int) number of nodes to plot
+        """
+        # 공부 더 필요
+        self.G = nx.Graph()
+        self.matrix = matrix
+
+    def creat_model(self, NETWORK_MAX):
+        # MST(Minum Spanning Tree)-based graph
+        # create edge
+        for i in range(len(self.matrix)):
+            # print('{0} is the number'.format(len(matrix)))
+            # print(matrix['Linkage'][i])
+            w1 = self.matrix['Linkage'][i][0]
+            w2 = self.matrix['Linkage'][i][1]
+            count = self.matrix['Weight'][i]
+            i += 1
+            if i > NETWORK_MAX: break
+
+            self.G.add_edge(w1, w2, weight=count)
+
+        # create MST model
+        self.T = nx.minimum_spanning_tree(self.G)
+        nodes = nx.nodes(self.T)
+        degrees = nx.degree(self.T)
+        # set size of node
+        self.node_size = []
+        for node in nodes:
+            ns = degrees[node] * 100
+            self.node_size.append(ns)
+        self.pos = nx.fruchterman_reingold_layout(self.G, k=0.5)
+
+    def vis_plt(self):
+        # matplotlib 그래프 생성
+
+        # font configuration
+        if sys.platform in ["win32", "win64"]:
+            font_name = "malgun gothic"
+        elif sys.platform == "darwin":
+            font_name = "AppleGothic"
+
+        # disply the result
+        plt.figure(figsize=(16, 12))
+        nx.draw_networkx(self.T,
+                         pos=self.pos,
+                         node_size=self.node_size,
+                         node_color="yellow",
+                         font_family=font_name,
+                         label_pos=1,  # 0=head, 0.5=center, 1=tail
+                         with_labels=True,
+                         font_size=12
+                         )
+
+        # 그래프 생성
+        plt.axis("off")
+        plt.show()
+
+        return self.G
+
+    def vis_plotly(self):
+        """ Note : Only work in web"""
+        # create edges
+        E = [e for e in self.G.edges]
+        pos = self.pos
+        # frutcherman layout == matrix
+
+        Xv = [pos[k][0] for k in range(len(self.matrix))]
+        Yv = [pos[k][1] for k in range(len(self.matrix))]
+        Xed = []
+        Yed = []
+        for edge in E:
+            Xed += [pos[edge[0]][0], pos[edge[1]][0], None]
+            Yed += [pos[edge[0]][1], pos[edge[1]][1], None]
+
+        trace3 = Scatter(x=Xed,
+                         y=Yed,
+                         mode='lines',
+                         line=dict(color='rgb(210,210,210)', width=1),
+                         hoverinfo='none'
+                         )
+        trace4 = Scatter(x=Xv,
+                         y=Yv,
+                         mode='markers',
+                         name='net',
+                         marker=dict(symbol='circle-dot',
+                                     size=5,
+                                     color='#6959CD',
+                                     line=dict(color='rgb(50,50,50)', width=0.5)
+                                     ),
+                         text=label,
+                         hoverinfo='text'
+                         )
+
+        annot = "This networkx.Graph has the Fruchterman-Reingold layout<br>Code:" + \
+                "<a href='http://nbviewer.ipython.org/gist/empet/07ea33b2e4e0b84193bd'> [2]</a>"
+
+        data1 = [trace3, trace4]
+        fig1 = Figure(data=data1, layout=layout)
+        fig1['layout']['annotations'][0]['text'] = annot
+        py.iplot(fig1, filename='Coautorship-network-nx')
+
+        return self.G
+
+    def save_graph(self):
+        nx.write_gexf(self.G, "Donald.gexf")
+
+
+N = Visualization(final_result)
+N.creat_model(len(final_result))
+N.vis_plt()
+N.save_graph()
+
+
+#
 # 척도 계산하기
 class Measure:
     def __init__(self, graph_ex):
@@ -186,6 +258,7 @@ class Measure:
         result = X / gg
         print(result)
         return result
+
     #
     def Get_Value(self):
         """
@@ -198,21 +271,17 @@ class Measure:
         clo_val = self.clo_GroupVal()
         bet_val = self.bet_GroupVal()
 
-
         return deg_val, clo_val, bet_val
 
 
-result = Measure(graph_ex)
-print(result.Get_Value())
+# result = Measure(graph_ex)
+# print(result.Get_Value())
 
 """ To Do
 
 객체화 - 진행중
-Graph 객체로 바꿔서 super 로 이어주
-
+Visual 이랑 Measure 랑 객체로 바꿔서 super 로 이어주기
 GroupVal 함수 하나로 통합해서 어떤 척도 계산할지만 입력하도록 하기 - 대충해놓음
 
-
 return, input 정확히 쓰기
-영어로 해야하나
 """
