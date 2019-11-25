9dab354eb1976a5348fba9ea4037dafe3440c822
JinUk, Cho <cju2725@gmail.com>
2019-11-26 00:43:49 +0900

    fix init issue

--- coocurrence.py
+++ coocurrence.py
@@ -23,7 +23,6 @@ import pandas as pd
 import os
 import ast
 
-
 from nltk import sent_tokenize, word_tokenize, pos_tag, ne_chunk
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
@@ -31,7 +30,6 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tree import Tree
 
 from merge_colloc import MergeColloc
-
 
 
 def strToList(df):
@@ -45,47 +43,42 @@ def strToList(df):
     return df
 
 class Processing():
-    def __init__(self, tag_filter):
+    def __init__(self):
         self.tag_map = defaultdict(lambda: wn.NOUN)
         self.tag_map['J'] = wn.ADJ
         self.tag_map['V'] = wn.VERB
         self.tag_map['R'] = wn.ADV
-        self.tag_filter = tag_filter
 
     @staticmethod
     def apply_collocations(sentence):
         sentence = sentence.replace("George H.W. Bush", "George_H.W._Bush")
+        sentence = sentence.replace("Donald J. Trump", "Donald_J._Trump")
         # add any phrase here
         return sentence
 
     # 문장 하나 lemmatization 함수
-    def lemma_sentence(self, text):
-        '''
-        lemmatize a sentence. If the pos tag of a token starts with 'V', 'R, 'J',
-        It will be lemmatized to the word's origin (was -> be) (studied -> study)
-        :param text: (string) a sentence
-        :return: (list) list of tokens
-        '''
+    def lemma_sentence(self, text):  # token에 is, 같은 애들을 be 로 변환 시키지 않음
         results = []
         tokens = word_tokenize(text)
-        # NER_chunk 함수 넣어주기
-        tokens = self.ner_chunk(tokens)  # -> pos tag / ne_chunk 포함
+        # NER_chunk 함수 넣어주기 (tokens 단위?)
+        tokens = self.ner_chunk(tokens)  #-> pos tag / ne_chunk 포함
         lmtzr = WordNetLemmatizer()
-        replace_data = {"n't": 'not'}  # lemmatatization에서 제거 되고 싶지 않은 단어 추가
+        replace_data={"n't":'not'} #lemmatatization에서 제거 되고 싶지 않은 단어 추가
         for token, tag in pos_tag(tokens):
+
+            # print("token :", token, "tag :", tag)
             if token in replace_data.keys():
-                token = token.replace(token, replace_data[token])
+                # print("pass replace_Data: ",token)
+                token =token.replace(token,replace_data[token])
+                # print("after replace: ",token)
             lemma = lmtzr.lemmatize(token, self.tag_map[tag[0]])
+            # print(token, "=>", lemma)
             results.append(lemma)
         return results
 
     # 문서 전체 lemmatization 함수
     def lemma_text(self, text):
-        '''
-        lemmatize text
-        :param text: (string) raw text
-        :return: (list of list) tokenized and lemmatized sentences
-        '''
+        # collocation 을 이 단에서 추가해야할 듯 (sent tokenize 되지 않도록)
         lemma_data = []
         sentences = sent_tokenize(text)
         for sent in sentences:
@@ -96,43 +89,37 @@ class Processing():
     # 불용어 처리 함수
     # 여기서부턴 string형태가 아니라 이중리스트 형태이므로 sentences 와 sentence 로 구분함
     def stopword(self, sentences):
-        '''
-        remove stopwords from sentences.
-        Note that 'not' is remained due to represent negation
-        :param sentences: (list of list)
-        :return: (list of list)
-        '''
-        stopWords = set(stopwords.words('english')) - set(['not'])
+        stopWords = set(stopwords.words('english'))-set(['not'])
         added_stopword = ['“', '”', '.', ',', '-', "—", "–", "'s", "n't", "''", ';', '&', "``", '?', "‘", "’"]
         results = []
 
-        for sent in sentences:
+        for sentence in sentences:
             wordsFiltered = []
             wordsStopped = []
-            for w in sent:
+            for w in sentence:
                 if w not in stopWords and w not in added_stopword and not w.isdigit():
                     wordsFiltered.append(w)
                 else:
                     wordsStopped.append(w)
             results.append(wordsFiltered)
 
+        # print(results)
         return results
 
     # 태깅 함수
 
     # apply_collocation 수정
-    def ner_chunk(self, tokens):  # George H.W. Bush는 따로 작업
-        '''
-        :param tokens: list of tokens in one sentence
-        :return:
-        '''
+    def ner_chunk(self,tokens): #George H.W. Bush는 따로 작업
         chunked = ne_chunk(pos_tag(tokens), binary=True)
+        # prev = None
         continuous_chunk = []
         current_chunk = []
-
+        # print("gcc_text in :", text)
+        # print("gcc chunked in : ",chunked )
         for i in chunked:
             if type(i) == Tree:
                 current_chunk.append("_".join([token for token, pos in i.leaves()]))
+                # print("current chunk: ",current_chunk,"\n")
                 named_entity = " ".join(current_chunk)
                 continuous_chunk.append(named_entity)
                 current_chunk = []
@@ -153,8 +140,8 @@ class Processing():
         :return: (list) tagged words divided by each sentence
         """
         results = []
-        for content in sentences:
-            tagged_content = pos_tag(content)
+        for sentence in sentences:
+            tagged_content = pos_tag(sentence)
             results.append(tagged_content)
 
         return results
@@ -174,7 +161,7 @@ class Processing():
             selection = []
             # 단어를 lex, tag 를 cat 이라 표현
             for lex, tag in sentence:
-                if tag in self.tag_filter:
+                if tag.startswith('V') or tag.startswith('N') or tag.startswith('R') or tag.startswith('J') :
                     # tag 말고 안에 단어 lex 만 남겨야함
                     selection.append(lex)
 
@@ -186,7 +173,7 @@ class Processing():
     def create_cooc_mat(self, sentences):
         """
         Create Co-Occurrence Matrix
-        :param sentences: (list of list) processed sentences.
+        :param sentences: (list of list) processed data
         :return: (list) The number of times two words occur together in each sentence in a document. [(word1, word2), count]
         """
         word_cooc_mat = Counter()
@@ -198,8 +185,12 @@ class Processing():
                     continue
                 elif word_cooc_mat[(w2, w1)] >= 1:  # 동일한 조합이고 순서만 반대인 경우
                     word_cooc_mat[(w2, w1)] += 1  # 처음 했던 조합에 카운트하겠다
+                    # print(word_cooc_mat[(w2, w1)])
                 else:
                     word_cooc_mat[(w1, w2)] += 1
+                    # print(word_cooc_mat[(w1, w2)])
+
+        # list_key_value = [[k,v] for k, v in word_cooc_mat.items()]
 
         list_keys = [k for k in word_cooc_mat.keys()]
         list_values = [v for v in word_cooc_mat.values()]
@@ -208,6 +199,7 @@ class Processing():
         weights = pd.Series(list_values)
         data = pd.DataFrame({'Linkage': linkages, 'Weight': weights})
         sorted_data = data.sort_values(by=['Weight'], ascending=False)
+        # return list_key_value
         return sorted_data
 
     def cooc(self, filepath=None, text=None, savepath=None):
